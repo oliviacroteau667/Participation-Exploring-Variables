29f1a4716073060ba9846b0a682e18831afcf425
Olivia Croteau (RIT Student) <111761829+oliviacroteau667@users.noreply.github.com>
2024-09-26 08:46:32 -0400

    Initial commit

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables
+            string heroFirstName = "Olivia";
+            string heroLastName = "Croteau";
+            string heroFullName;
+            string villianTitle = "Lord of Darkness";
+            string minionTitle = "Background Grunt";
+            string villianName;
+            string minion1Name;
+            string minion2Name;
+
+            int heroHealth = 100;
+            int heroStrength = 50;
+            int bossHealth = 100;
+            int bossStrength = 25;
+            int minion1Health = 50;
+            int minion2Health = 50;
+            int minionStrength = 5;
+
+            //Begin Code Instructions
+            heroFullName = heroFirstName + " " + heroLastName;
+            // Console.WriteLine("Behold the glorious hero!");
+            // Console.WriteLine(heroFullName);
+            Console.WriteLine("Behold the legendary hero of programming, whose name is: " +  heroFullName);
+
+            villianName = "Steve";
+            minion1Name = "Joe";
+            minion2Name = "Shmoe";
+
+
+            Console.WriteLine(heroFullName + "'s arch rival is " + villianName + " the " + villianTitle);
+            Console.WriteLine(villianName + " the " + villianTitle + " is helped out by: " + minionTitle + " " + minion1Name + " and " + minionTitle + " " + minion2Name);
+
+            Console.WriteLine("The fighters enter the room!");
+            Console.WriteLine("Hero's health: " + heroHealth);
+            Console.WriteLine("Boss' health: " + bossHealth);
+            Console.WriteLine("Minion 1's health: " + minion1Health);
+            Console.WriteLine("Minion 2's health: " + minion2Health);
+
+            Console.WriteLine(villianName + " the " + villianTitle + " attacks " + heroFullName);
+            Console.WriteLine(villianName + " the " + villianTitle + " deals " + bossStrength + " damage!");
+
+            heroHealth = heroHealth - bossStrength;
+            Console.WriteLine(heroFullName + " now has " + heroHealth + " health!");
+
+            Console.WriteLine("The evil minions attack!");
+            Console.WriteLine(minion1Name + " deals " + minionStrength + " damage!");
+
+            heroHealth = heroHealth - minionStrength;
+            Console.WriteLine(heroFullName + " now has " + heroHealth + " health!");
+            Console.WriteLine(minion2Name + " deals " + minionStrength + " damage!");
+
+            heroHealth = heroHealth - minionStrength;
+
+            Console.WriteLine(heroFullName + " attacks each villian for " + heroStrength + " damage!");
+
+            bossHealth -= heroStrength;
+            minion1Health -= heroStrength;
+            minion2Health -= heroStrength;
+
+            Console.WriteLine(villianName + " the " + villianTitle + " now has " + bossHealth + " health!");
+            Console.WriteLine(minionTitle + " " + minion1Name + " now has " + minion1Health + " health!");
+            Console.WriteLine(minionTitle + " " + minion2Name + " now has " + minion2Health + " health!");
+
+            Console.WriteLine(minion1Name + " and " + minion2Name + " have been defeated!");
+
+            Console.WriteLine(heroFullName + " has " + heroHealth + " health");
+            Console.WriteLine(heroFullName + " eats an apple to restore health!");
+            heroHealth++;
+            Console.WriteLine(heroFullName + " now has " + heroHealth + " health!");
+
+            Console.WriteLine("The remaining fighters continue the fight!");
+            Console.WriteLine("Hero's health: " + heroHealth);
+            Console.WriteLine("Boss's health: " + bossHealth);
+
+            Console.WriteLine(villianName + " the " + villianTitle + " attacks " + heroFullName);
+            Console.WriteLine(villianName + " the " + villianTitle + " deals " + bossStrength + " damage!");
+
+            heroHealth = heroHealth - bossStrength;
+            Console.WriteLine(heroFullName + " now has " + heroHealth + " health!");
+
+            Console.WriteLine(heroFullName + " attacks " + villianName + " the " + villianTitle + " for " + heroStrength + " damage!");
+
+            bossHealth = bossHealth - heroStrength;
+
+            Console.WriteLine(villianName + " the " + villianTitle + " now has " + bossHealth + " health!");
+
+            Console.WriteLine(heroFullName + " is victorious!");
+            Console.WriteLine("Wealth, glory, and straight A+'s to the winner!");
+        }
+    }
+}
